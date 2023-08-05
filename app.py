2be11d9c5455d33ff97eb2bd111515d7da7e5284
mimiprograms <kahmobey@gmail.com>
2023-08-05 00:29:42 -0500

    feat: major updates to ui

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+def home():
+    return render_template('card.html')
+
+# app.run(debug=True, host='192.168.1.214')
