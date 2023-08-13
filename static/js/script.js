25ab7e0172d7669db9fbf6304b75e67acfdc7a5b
mimiprograms <kahmobey@gmail.com>
2023-08-13 14:46:31 -0500

    initial commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 /* TO-DO: 
 
-* make buttons functional
+* add button for range
 
 * user select custom themes
 
@@ -8,3 +8,50 @@
 
 
 */
+
+
+// #region login button
+var loginBtn = document.getElementById('loginBtn');
+if (loginBtn){
+    var isLoggedIn = loginBtn.getAttribute('data-isloggedin');
+    var loginRoute = loginBtn.getAttribute('data-login-route');
+    var genRoute = loginBtn.getAttribute('data-gen-route');
+    loginBtn.innerText = 'Login with Spotify';
+
+    if (isLoggedIn === 'true') {
+        loginBtn.innerText = 'Generate';
+        loginBtn.addEventListener('click', function() {
+            window.location.href = genRoute;
+        });
+    } else {
+    
+    loginBtn.addEventListener('click', function() {
+        window.location.href = loginRoute;
+    });}
+}
+// #endregion
+
+
+ // #region get the current/future date
+ var currentDate = document.querySelector("#currentDate");
+ if (currentDate){
+    var today = new Date();
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var formattedDate = today.getDate().toString().padStart(2, '0') + ' ' + monthNames[today.getMonth()] + ' ' + today.getFullYear();
+    console.log(formattedDate);
+    currentDate.textContent = formattedDate;
+ }
+
+ var futureDate = document.querySelector("#futureDate");
+ if (futureDate){
+    var today = new Date();
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var formattedDate = today.getDate().toString().padStart(2, '0') + ' ' + monthNames[today.getMonth()] + ' 20XX';
+    console.log(formattedDate);
+    futureDate.textContent = formattedDate;
+ }
+
+
+ // #endregion
+
+ 
